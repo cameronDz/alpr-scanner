57ba6d7870ae27f1f449bba88c86e06aacb7cea0
Anthony <brignano@my.ccsu.edu>
2016-03-03 22:36:56 -0500

    Updated CameraActivity.java
    - added "pinch-zoom" control to CameraActivity.java

--- app/src/main/java/org/openalpr/app/CameraActivity.java
+++ app/src/main/java/org/openalpr/app/CameraActivity.java
@@ -18,6 +18,7 @@ import android.net.Uri;
     import android.support.v4.app.ActivityCompat;
     import android.support.v4.content.ContextCompat;
     import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.WindowManager;
     import android.os.Bundle;
     import android.util.Log;
@@ -43,7 +44,8 @@ import java.io.FileOutputStream;
     import java.io.IOException;
     import java.text.SimpleDateFormat;
     import java.util.Date;
-    import java.util.Locale;
+import java.util.List;
+import java.util.Locale;
 
 /**
  *
@@ -87,6 +89,7 @@ public class CameraActivity extends AppCompatActivity implements
     private GoogleApiClient mGoogleApiClient;
     private LatLng mLatLng = null;
     private Location mLastLocation = null;
+    private float mDist = 0;
 
     private String tempFilePath;
 
@@ -494,6 +497,75 @@ public class CameraActivity extends AppCompatActivity implements
     public void onConnectionFailed(ConnectionResult cr){
         // code to be run when connection fails
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Get the pointer ID
+        Camera.Parameters params = mCamera.getParameters();
+        int action = event.getAction();
+
+
+        if (event.getPointerCount() > 1) {
+            // handle multi-touch events
+            if (action == MotionEvent.ACTION_POINTER_DOWN) {
+                mDist = getFingerSpacing(event);
+            } else if (action == MotionEvent.ACTION_MOVE && params.isZoomSupported()) {
+                mCamera.cancelAutoFocus();
+                handleZoom(event, params);
+            }
+        } else {
+            // handle single touch events
+            if (action == MotionEvent.ACTION_UP) {
+                handleFocus(event, params);
+            }
+        }
+        return true;
+    }
+
+    private void handleZoom(MotionEvent event, Camera.Parameters params) {
+        int maxZoom = params.getMaxZoom();
+        int zoom = params.getZoom();
+        float newDist = getFingerSpacing(event);
+        if (newDist > mDist) {
+            //zoom in
+            if (zoom < maxZoom)
+                zoom++;
+        } else if (newDist < mDist) {
+            //zoom out
+            if (zoom > 0)
+                zoom--;
+        }
+        mDist = newDist;
+        params.setZoom(zoom);
+        mCamera.setParameters(params);
+    }
+
+    public void handleFocus(MotionEvent event, Camera.Parameters params) {
+        int pointerId = event.getPointerId(0);
+        int pointerIndex = event.findPointerIndex(pointerId);
+        // Get the pointer's current position
+        float x = event.getX(pointerIndex);
+        float y = event.getY(pointerIndex);
+
+        List<String> supportedFocusModes = params.getSupportedFocusModes();
+        if (supportedFocusModes != null && supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+            mCamera.autoFocus(new Camera.AutoFocusCallback() {
+                @Override
+                public void onAutoFocus(boolean b, Camera camera) {
+                    // currently set to auto-focus on single touch
+                }
+            });
+        }
+    }
+
+    /** Determine the space between the first two fingers */
+    private float getFingerSpacing(MotionEvent event) {
+        // ...
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
 
     protected void createLocationRequest() {
         /**
