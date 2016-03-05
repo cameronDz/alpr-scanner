26225032e9237b766ed7dbd9e07c66c2672435b0
cameronDz <cameron.dziurgot@my.ccsu.edu>
2016-03-04 22:00:27 -0500

    Added GCM dependencies and plugin's to Project and App grandle.build files. Added GCM permissions to Manifest. Added Constants class, and GCM classes. Currently obtain InstanceID and registration token when the LoginActivity is opened. When Login button is clicked, an attempt to send a dummy message to the GCM server is made. Does not reach the server though. No errors thrown.

--- app/src/main/java/org/openalpr/app/LoginActivity.java
+++ app/src/main/java/org/openalpr/app/LoginActivity.java
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
     private Context context;
+    protected GoogleCloudMessaging gcm = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,31 +41,29 @@ public class LoginActivity extends AppCompatActivity {
         context = getApplicationContext();
 
         // Register an InstanceID
-        Log.v("GCM_REGISTER", "BEFORE");
+        Log.v(TAG, "GCM_REGISTER BEFORE " + Constants.INST_ID);
         Constants.INST_ID =  InstanceID.getInstance(context).getId();
-        Log.v("GCM_REGISTER", "AFTER " + Constants.INST_ID);
+        Log.v(TAG, "GCM_REGISTER AFTER " + Constants.INST_ID);
 
 
         // Get token
         new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... params) {
-                String msg = "FAIL";
-                Log.v("GCM_TOKE", "BEFORE");
-                String authorizedEntity = Constants.SENDER_ID;
+                String msg = "";
+                Log.v(TAG, "GCM_TOKE BEFORE");
+                String authorizedEntity = Constants.PROJECT_ID;
                 String scope = "GCM";
                 try {
-                    Log.v("GCM_TOKE", "BEFORE_IN_TRY");
+                    Log.v(TAG, "GCM_TOKE BEFORE_IN_TRY");
                     Constants.REG_TOKEN = InstanceID.getInstance(context).getToken(authorizedEntity, scope);
                     msg = Constants.REG_TOKEN;
-                    Log.v("GCM_TOKE", "AFTER_IN_TRY");
+                    Log.v(TAG, "GCM_TOKE AFTER_IN_TRY, Reg_Token: " + Constants.REG_TOKEN);
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.v("GCM_TOKE", "FAIL FAIL FAIL FAIL");
-                    Log.v("GCM_TOKE", "FAIL FAIL FAIL FAIL");
-                    Log.v("GCM_TOKE", "FAIL FAIL FAIL FAIL");
+                    Log.v(TAG, "GCM_TOKE FAIL FAIL FAIL FAIL");
                 }
-                Log.v("GCM_TOKE", "AFTER");
+                Log.v(TAG, "GCM_TOKE AFTER");
 
                 return msg;
             }
@@ -100,8 +99,8 @@ public class LoginActivity extends AppCompatActivity {
 
 
         //gcm sends hi message to server
-        Constants.gcm = GoogleCloudMessaging.getInstance(this);
-        Log.v("GCM_PRINT", Constants.gcm.toString());
+        gcm = GoogleCloudMessaging.getInstance(context);
+        Log.v("GCM_PRINT gcm: ", gcm.toString());
         new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... params) {
@@ -116,15 +115,13 @@ public class LoginActivity extends AppCompatActivity {
                     data.putString("my_action5", "SAY_HELLO");
                     String id = Integer.toString(Constants.MSG_ID) + "alpr";
                     Constants.MSG_ID++;
-                    Log.v("GCM_SEND", "BEFORE");
-                    Log.v("GCM_SEND", "BEFORE_TOKEN: " + Constants.REG_TOKEN);
-                    Log.v("GCM_SEND", "BEFORE_PROJECT_ID: " + Constants.PROJECT_ID);
-                    Constants.gcm.send(Constants.PROJECT_ID + "@gcm.googleapis.com", id, data);
-                    Log.v("GCM_SEND", "AFTER_data: " + data.toString());
-                    Log.v("GCM_SEND", "AFTER");
+                    Log.v(TAG, "GCM_SEND BEFORE_TOKEN: " + Constants.REG_TOKEN);
+                    Log.v(TAG, "GCM_SEND BEFORE_PROJECT_ID: " + Constants.PROJECT_ID);
+                    gcm.send(Constants.PROJECT_ID + "@gcm.googleapis.com", id, data);
+                    Log.v(TAG, "GCM_SEND AFTER_data: " + data.toString());
                 } catch (IOException ex) {
                     msg = "Error :" + ex.getMessage();
-                    Log.v("GCM_SEND", "Error");
+                    Log.v(TAG, "GCM_SEND Error");
                 }
                 return msg;
             }
@@ -134,7 +131,6 @@ public class LoginActivity extends AppCompatActivity {
                 // mDisplay.append(msg + "\n");
             }
         }.execute(null, null, null);
-
 
         // Logic needs to be added to this variable based on the database interaction(s)
         Boolean loginSuccess = true;
